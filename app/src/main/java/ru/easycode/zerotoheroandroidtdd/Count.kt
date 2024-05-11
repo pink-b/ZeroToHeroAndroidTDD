40f9e890930ed4950188a63188b503562050026b
pink-b <60101962+pink-b@users.noreply.github.com>
2024-05-11 18:09:32 +0400

    Added a solution to the task

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,21 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+
+    fun increment(number: String): String
+    class Base(private val step: Int): Count {
+
+        init {
+            if (step < 1) {
+                throw IllegalStateException("step should be positive, but was $step")
+            }
+        }
+        override fun increment(number: String): String {
+            var newNum = number.toInt()
+            newNum += step
+            return newNum.toString()
+        }
+
+    }
+
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val count = Count.Base(2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.incrementButton)
+        val text = findViewById<TextView>(R.id.countTextView)
+
+
+        button.setOnClickListener {
+            val newValue = count.increment(text.text.toString())
+            text.text = newValue
+        }
     }
 }
