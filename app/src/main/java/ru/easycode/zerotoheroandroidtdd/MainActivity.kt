4e9fe32cde03b87fd3f3ba7798201e7ecbddb26d
pink-b <60101962+pink-b@users.noreply.github.com>
2024-05-25 17:48:02 +0400

    with handler

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,7 +2,10 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -26,22 +29,33 @@ class MainActivity : AppCompatActivity() {
 
 //        stateChanger.initState(loadButton, text, progressBar)
 
-        if (viewModel.state.value == null) {
-            Log.d("Sergey", "YRAAAA")
-            viewModel.changeLoadState(ChangeState.InitState())
-        }
+//        if (viewModel.state.value == null) {
+//            Log.d("Sergey", "YRAAAA")
+//            viewModel.changeLoadState(ChangeState.InitState())
+//        }
+//
+//        loadButton.setOnClickListener {
+//            viewModel.changeLoadState(ChangeState.LoadState())
+//            loadButton.postDelayed({
+//                viewModel.changeLoadState(ChangeState.LoadedState())
+//            }, 3500)
+//
+//        }
+//
+//        viewModel.state.observe(this, Observer {
+//            it.apply(loadButton, text, progressBar)
+//        })
 
-        loadButton.setOnClickListener {
-            viewModel.changeLoadState(ChangeState.LoadState())
-            loadButton.postDelayed({
-                viewModel.changeLoadState(ChangeState.LoadedState())
-            }, 3500)
+            loadButton.setOnClickListener {
+                loadButton.isEnabled = false
+                progressBar.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed({
+                    text.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    loadButton.isEnabled = true
+                }, 3500)
+            }
 
-        }
-
-        viewModel.state.observe(this, Observer {
-            it.apply(loadButton, text, progressBar)
-        })
     }
 
 
